3993a77d5027fedfec0a7c99f9366aaced58c2fe
Matthew LettucePie <thelettucepie@gmail.com>
2023-10-14 13:48:42 -0400

    Closest setup using rigidbody, going to have to make it work

--- gameplay/player.gd
+++ gameplay/player.gd
@@ -1,7 +1,6 @@
 extends RigidBody2D
 
-@export var hold_mass : float = 10000.0
-@export var release_mass : float = 1.0
+@export var sticky = false
 
 var sticky_ramp : Ramp
 var ramp_step : Array = [true, Vector2.ZERO, 0]
@@ -13,18 +12,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if sticky_ramp != null:
+	if sticky_ramp != null and sticky:
 		ramp_step = sticky_ramp.return_next_step(self.position, ramp_step[2])
 		if ramp_step[0]:
 			print("Pressing against ramp")
-#				mass = 1.0
 			apply_central_force(ramp_step[1] * 500)
 	if Input.is_action_pressed("ui_accept"):
 		apply_torque(20000.0)
+	else:
+		apply_torque(1000.0)
 
 
 func _on_body_entered(body):
 	if body.is_in_group("ramp") and sticky_ramp != body:
 		print("Sticking to ramp: ", body)
 		sticky_ramp = body
-#		mass = 0.1
