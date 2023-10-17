f4cbee341ecd7d8073338b141e021cfa697e3b30
Matthew LettucePie <thelettucepie@gmail.com>
2023-10-17 02:52:25 -0400

    Added backup signal connections

--- levels/launch.gd
+++ levels/launch.gd
@@ -3,6 +3,15 @@ extends Path2D
 class_name Launch
 
 
+func _ready():
+	var player = get_tree().get_nodes_in_group("player")[0]
+	if player != null:
+		if !player.is_connected("launch_curve", _on_player_follow_launch_curve):
+			player.connect("launch_curve", _on_player_follow_launch_curve)
+		if !player.is_connected("parent_launch", _on_player_follow_parent_launch):
+			player.connect("parent_launch", _on_player_follow_parent_launch)
+
+
 func _on_player_follow_launch_curve(new_curve):
 	set_curve(new_curve)
 	$preview.points = new_curve.get_baked_points()
