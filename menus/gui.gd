9d4be7b7941539317d385bbed3021b6e682c8d59
Matthew LettucePie <thelettucepie@gmail.com>
2023-10-19 18:57:17 -0400

    Formated time and travel

--- menus/gui.gd
+++ menus/gui.gd
@@ -73,18 +73,24 @@ func _process(delta):
 		emit_signal("player_pause")
 
 
+func time_from_msecs(msecs):
+	var seconds = msecs / 1000
+	var minutes = round(seconds / 60)
+	return str(minutes) + ":" + str(seconds)
+
+
 func update_labels():
 	if playing:
-		$Panel/VBoxContainer/time_container/time.text = str(Time.get_ticks_msec() - stage.start_time)
+		$Panel/VBoxContainer/time_container/time.text = time_from_msecs(Time.get_ticks_msec() - stage.start_time)
 		$Panel/VBoxContainer/score_container/score.text = str(stage.score)
 		$TextureRect/speedometer.value = speedometer[player.speed_stage]
 		$TextureRect/momentum.value = player.speed_shift
 
 
 func display_results():
-	$results_panel/VBoxContainer/time_container/time_result.text = str(stage.final_time)
+	$results_panel/VBoxContainer/time_container/time_result.text = time_from_msecs(stage.final_time)
 	$results_panel/VBoxContainer/score_container/score_result.text = str(stage.score)
-	$results_panel/VBoxContainer/travel_container/travel_result.text = str(stage.travel)
+	$results_panel/VBoxContainer/travel_container/travel_result.text = str(round(stage.travel) / 3) + "m"
 
 
 func _on_start_button_pressed():
