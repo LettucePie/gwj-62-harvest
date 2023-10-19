90ffc009986754e8a84c22f866351609ebe111fc
Matthew LettucePie <thelettucepie@gmail.com>
2023-10-19 11:31:02 -0400

    Added base gui to gameplay, need to fix brakes

--- gameplay/ui/testing_ui.gd
+++ gameplay/ui/testing_ui.gd
@@ -9,10 +9,10 @@ func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player != null and stage != null:
 		update_labels()
+
 
 func update_labels():
 	$Panel/VBoxContainer/status_container/status.text = str(player.status)
---
+++ menus/gui.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+var stage : Stage
+var player : Player
+
+var playing : bool = true
+var speedometer : Array = [10, 25, 45, 75, 100]
+
+func _ready():
+	$results_panel.hide()
+	$ready_panel.hide()
+	stage = get_tree().get_nodes_in_group("stage")[0]
+	player = get_tree().get_nodes_in_group("player")[0]
+
+
+func _process(delta):
+	if player != null and stage != null:
+		update_labels()
+
+
+func update_labels():
+	if playing:
+		$Panel/VBoxContainer/time_container/time.text = str(Time.get_ticks_msec() - stage.start_time)
+		$Panel/VBoxContainer/score_container/score.text = str(stage.score)
+		$TextureRect/speedometer.value = speedometer[player.speed_stage]
+		$TextureRect/momentum.value = player.speed_shift
+#	$Panel/VBoxContainer/status_container/status.text = str(player.status)
+#	$Panel/VBoxContainer/angle_container/angle.text = str(rad_to_deg(player.angle))
+#	$Panel/VBoxContainer/vert_container/vert.text = str(player.vert_intensity)
+#	$Panel/VBoxContainer/speed_shift/speed.text = str(player.speed_shift)
+#	$Panel/VBoxContainer/speed_stage/stage.text = str(player.speed_stage)
+#	$Panel/VBoxContainer/travel_dist/travel.text = str(stage.travel)
+#	$Panel/VBoxContainer/score_total/score.text = str(stage.score)
